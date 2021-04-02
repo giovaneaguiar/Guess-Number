f0ac5d7206c23cd823bfecfcc4756e3858864c54
Giovane Aguiar <giovaneaguiar@ice.ufjf.br>
2021-04-02 16:32:41 -0300

    Analisa todo código e Finaliza JS.

--- js/index.js
+++ js/index.js
@@ -7,12 +7,12 @@ let result = document.getElementById('result');
 
 const Guess = {
     max: 10,
-    attemptsNumber = 0,
+    attemptsNumber: 0,
     numberDrawn: function randomValue() {
         //Math random gera um valor aleatório de acordo com o máximo permitido;
         //No nosso caso, a variável max possui 10.
         //Math.round arrendonda os valores, ou seja, não vem valor decimal (quebrado).
-        return Math.round(Math.random()*this.max);
+        return Math.round(Math.random() * this.max);
     }
 
 };
@@ -30,12 +30,31 @@ function handleSubmit(e) {
       let kick = document.getElementById('kick').value;
       if(!kick){
       //Se eu não tiver um chute, não deixar o usuário continuar e manda um alerta.
-      alert('Digite algum valor!');
+      alert('Digite algum valor!')
       return;
       };
 
       updateAttempt(attempt, ++Guess.attemptsNumber);
-
+      
+      if(numberDrawn == kick) {
+          //Se o número que a máquina pensou for igual ao chute do usuário
+          playAgain();
+          status.innerHTML = 'Parabéns, você acertou!';
+          result.style.transition = '0.4s';
+          result.style.backgroundColor = '#3FBF3F';
+          result.style.color = '#fff';
+          status.style.color = '#fff';
+          clear();
+      } else if(numberDrawn > kick){
+      //Se o número que a máquina pensou for maior que o chute do usuário
+      status.innerHTML = 'O número é maior!';
+      status.style.color =  '#FF0000';
+      clear();
+      } else if (numberDrawn < kick){
+        status.innerHTML = 'O número é menor!';
+        status.style.color =  '#FF0000';
+        clear();
+      }
 };
 
 // Função para mostrar o botão de jogar novamente.
